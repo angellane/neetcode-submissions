f1a7d9957ff1e8f648daaa7345eb23d64a50b7d8
fabian <fabianferreira36@gmail.com>
2026-08-18 18:46:47 +0100

    Add: min-cost-climbing-stairs - submission-0

---
+++ Data Structures & Algorithms/min-cost-climbing-stairs/submission-0.py
@@ -0,0 +1,10 @@
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        #cost[i] is the cost of taking a step from the ith floro 
+
+        def dfs(i):
+            if i >= len(cost):
+                return 0
+            return cost[i] + min(dfs(i + 1), dfs(i + 2))
+        
+        return min(dfs(0), dfs(1) )
