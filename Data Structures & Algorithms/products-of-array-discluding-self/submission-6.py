2067e3cb17880712091283c5699ab5484a42c099
fabian <fabianferreira36@gmail.com>
2026-08-26 20:30:32 +0100

    Add: products-of-array-discluding-self - submission-6

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-6.py
@@ -0,0 +1,24 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+
+        output = []
+        for i in range(len(nums)):
+            product = 1
+            l,r = 0, len(nums) -1
+
+            while l < i:
+                product *= nums[l]
+                l+=1
+
+            while r > i:
+                product *= nums[r]
+                r-=1
+            output.append(product)
+        return output
+
+#2 pointers works here too but its still too inefficient 
+
+        
+
+                    
+        
