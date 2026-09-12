5ebfe39ae7ede255542a9a6d6d545b5484051925
fabian <fabianferreira36@gmail.com>
2026-09-12 19:26:41 +0100

    Add: reorder-linked-list - submission-3

---
+++ Data Structures & Algorithms/reorder-linked-list/submission-3.py
@@ -0,0 +1,35 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        nodes = []
+
+        #now fill the array with the nodes
+
+        curr = head
+        while curr:
+            nodes.append(curr)
+            curr = curr.next
+
+        # now we want to set 2 pointers at the start and end of the list, 
+
+        l,r = 0, len(nodes) - 1
+
+        while l < r:
+            nodes[l].next = nodes[r]
+            l+=1
+            
+            nodes[r].next = nodes[l]
+            r-=1
+        nodes[l].next = None #this is so the last node doesnt still have it original next pointer value. Instead we make it None since it has reached the end of the list 
+
+
+# this solution adds the linkedlist into an array. Then we can set 2 pointers at the start and end of the array and find the next node for l and make it equal to the node at where the r pointer is. We also find the next node for the r pointer and make it equal to the l node. 
+
+
+        
+        
