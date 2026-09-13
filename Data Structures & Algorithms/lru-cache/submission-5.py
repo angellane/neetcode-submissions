7615cb483df046cc74ee1ff3475424ca42004028
fabian <fabianferreira36@gmail.com>
2026-09-13 17:10:07 +0100

    Add: lru-cache - submission-5

---
+++ Data Structures & Algorithms/lru-cache/submission-5.py
@@ -0,0 +1,36 @@
+class LRUCache:
+    #intialise hashmap and capacity
+    #perform the get operation, if pair is not in cache then return -1, if it is. remove and readd to show that it was 'used'
+    # perform the put operation, if the key exists update it to be the most recently used, if it isnt add it to cache anyway. At the end aswell if the cache is bigger than the capacity we are given then pop from the left until we are within range
+
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.cache = {}
+        
+
+    def get(self, key: int) -> int: #we must return an int
+        if key not in self.cache:
+            return -1 
+        value = self.cache.pop(key)
+        self.cache[key] = value 
+        return value 
+        
+
+    def put(self, key: int, value: int) -> None: # we return nothing
+        if key in self.cache:
+            self.cache.pop(key)
+        self.cache[key] = value 
+
+        if len(self.cache) > self.capacity:
+            lru = next(iter(self.cache))
+            del self.cache[lru]
+        
+
+
+
+
+#for this problem we could only ever be 1 size over the capacity. So the if condition satisfies. If it didnt however then we could do the following. 
+ #   while len(self.cache) > self.capacity:
+  #      lru = next(iter(self.cache))
+  #       del self.cache[lru]
+        
